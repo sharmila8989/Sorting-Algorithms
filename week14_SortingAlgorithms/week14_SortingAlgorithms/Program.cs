6f9a84b16efb8eee85ffd75508727847bc05d7b9
sharmila8989 <sharmila8989@users.noreply.github.com>
2018-07-03 21:49:45 +1000

    completed all sorts

--- week14_SortingAlgorithms/week14_SortingAlgorithms/Program.cs
+++ week14_SortingAlgorithms/week14_SortingAlgorithms/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace week14_SortingAlgorithms
 {
@@ -10,15 +12,85 @@ namespace week14_SortingAlgorithms
     {
         static void Main(string[] args)
         {
+            DateTime sTime, eTime;
+            List<int> unsortedList = fileReader();
+    
+            //Insertion Sorting
+            sTime = DateTime.Now;
+            List<int> insertionSList = Sorting_Algo.InsertionSort(unsortedList);
+            eTime = DateTime.Now;
+            Console.WriteLine("The insertion Sorting : " + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+            // Shell Sorting
+            sTime = DateTime.Now;
+            List<int> shellSList = Sorting_Algo.ShellSort(unsortedList);
+            eTime = DateTime.Now;
+            Console.WriteLine("The shell Sorting : " + ((eTime -sTime).TotalMilliseconds) + "milliseconds");
+
+            // Linear Search of greatest number in Insertion Sort
+            sTime = DateTime.Now;
+            int greatestNum = Sorting_Algo.LinearSearch(insertionSList);
+            eTime = DateTime.Now;
+            Console.WriteLine("The Linear Search for "+greatestNum+" of insertion sorted list took " + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+            // Linear Search of greatest number in Shell Sort
+            sTime = DateTime.Now;
+            int greatestNum2 = Sorting_Algo.LinearSearch(shellSList);
+            eTime = DateTime.Now;
+            Console.WriteLine("The Linear Search for " + greatestNum + " of shell sorted list took " + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+
+            // Binary Search of greatest number in Insertion Sort
+            sTime = DateTime.Now;
+            int index = Sorting_Algo.BinarySearch(insertionSList, greatestNum);
+            eTime = DateTime.Now;
+            Console.WriteLine("The Binary Search for " + greatestNum + " of insertion sorted list took " + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+
+            //Binary Search of greates number in Shell Sort
+            sTime = DateTime.Now;
+            int index1 = Sorting_Algo.BinarySearch(shellSList, greatestNum2);
+            eTime = DateTime.Now;
+            Console.WriteLine("The Binary Search for " + greatestNum + " of shell sorted list took " + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+
+            //Linear search of every 1500thnumber - insertion sorted List
+            sTime = DateTime.Now;
+            List<int> lSearch1500th = Sorting_Algo.LinearSearchEvery1500th(insertionSList);
+            eTime = DateTime.Now;
+            Console.WriteLine("The Linear Search for 1500th number Insertion sort took "  + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+
+            //Linear search of every 1500thnumber - shell sorted List
+            sTime = DateTime.Now;
+            List<int> lSearch1500th_shel = Sorting_Algo.LinearSearchEvery1500th(shellSList);
+            eTime = DateTime.Now;
+            Console.WriteLine("The Linear Search for 1500th number Shell sort took " + ((eTime - sTime).TotalMilliseconds) + "milliseconds");
+
+            // Press enter to exit
+            Console.ReadKey(true);
 
 
 
 
-
-         
         }
 
-       
+        static List<int> fileReader()
+        {
+            List<int> numList = new List<int>();
+
+            string[] numArray = System.IO.File.ReadAllLines(@"C:\Users\sharm\Source\Repos\Sorting-Algorithms\unsorted_numbers.csv");
+            List<string> nList = new List<string>(numArray);
+            foreach (string s in nList)
+            {
+                int i = int.Parse(s);
+                numList.Add(i);
+            }
+            return numList;
+        }
+
+
+
 
     }
 }
--- week14_SortingAlgorithms/week14_SortingAlgorithms/Sorting_Algo.cs
+++ week14_SortingAlgorithms/week14_SortingAlgorithms/Sorting_Algo.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace week14_SortingAlgorithms
 {
-    class Sorting_Algo
+    public class Sorting_Algo
     {
-        static List<int> InsertionSort(List<int> numberlist)
+        static public List<int> InsertionSort(List<int> numberlist)
         {
             int count = numberlist.Count;
 
@@ -29,7 +29,7 @@ namespace week14_SortingAlgorithms
             return numberlist;
         }
 
-        static List<int> ShellSort(List<int> numberlist)
+        static public List<int> ShellSort(List<int> numberlist)
         {
             // Algorithm sourced from http://www.csharpstar.com/shell-sort-csharp-program/
             int count = numberlist.Count;
@@ -58,17 +58,22 @@ namespace week14_SortingAlgorithms
             return numberlist;
         }
 
-        static int LinearSearch(List<int> numberList, int num)
+        public static int LinearSearch(List<int> numberList)
         {
-            // Algorithm sourced from https://www.geeksforgeeks.org/linear-search/
-            int i;
-            for (i = 0; i < numberList.Count; i++)
-                if (numberList[i] == num)
-                    return i;
-            return -1;
+           
+            int greatest = numberList[0];
+            for (int i = 0; i < numberList.Count; i++)
+            {
+                if (numberList[i] > greatest)
+                {
+                    greatest = numberList[i];
+                }
+
+            }
+            return greatest;
         }
 
-        static int BinarySearch(List<int> numberList, int num)
+        public static int BinarySearch(List<int> numberList, int num)
         {
             int low = 0;
             int mid;
@@ -92,7 +97,7 @@ namespace week14_SortingAlgorithms
             return -1;
         }
 
-        static List<int> LinearSearchEvery1500th (List<int> numList)
+        public static List<int> LinearSearchEvery1500th (List<int> numList)
         {
             List<int> aList = new List<int>();
             var sTime = DateTime.Now;
